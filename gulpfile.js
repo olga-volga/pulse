64183349f9e5b0baf1204551daad6ae84bc836c2
olga <yaolmi@yandex.ru>
2021-10-24 12:02:35 +0700

    optimize page load speed & modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,114 @@
-const gulp        = require('gulp');
+"use strict";
+
+const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const webpack = require("webpack-stream");
 
-gulp.task('server', function() {
+//const dist = "./dist";
 
-    browserSync({
-        server: {
-            baseDir: "src"
-        }
-    });
-
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+gulp.task('html', function() {
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest("dist/"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/scss/**/*.+(scss|sass)")
+    return gulp.src("src/scss/**/*.scss")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-    gulp.watch("src/scss/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+gulp.task('build-js', function() {
+    return gulp.src("src/js/main.js")
+        .pipe(webpack({
+            mode: 'development',
+            output: {
+                filename: 'script.js'
+            },
+            watch: false,
+            devtool: "source-map",
+            module: {
+                rules: [
+                    {
+                        test: /\.m?js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: true,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                            }
+                        }
+                    }
+                ]
+            }
+        }))
+        .pipe(gulp.dest("dist/"))
+        .on("end", browserSync.reload);
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('fonts', function() {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('images', function() {
+    return gulp.src("src/img/**/*")
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("watch", function() {
+    browserSync.init({
+        server: "dist/",
+        port: 4000,
+        notify: true
+    });
+    gulp.watch("src/*.html", gulp.parallel('html'));
+    gulp.watch("src/scss/**/*.+(scss|css)", gulp.parallel('styles'));
+    gulp.watch("src/fonts/**/*", gulp.parallel('fonts'));
+    gulp.watch("src/img/**/*", gulp.parallel('images'));
+    gulp.watch("src/js/**/*.js", gulp.parallel('build-js'));
+});
+
+gulp.task('build', gulp.parallel('html', 'styles', 'fonts', 'images', 'build-js'));
+
+gulp.task('build-prod-js', function() {
+    return gulp.src("src/js/main.js")
+        .pipe(webpack({
+            mode: 'production',
+            output: {
+                filename: 'script.js'
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.m?js$/,
+                        exclude: /(node_modules|bower_components)/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: [['@babel/preset-env', {
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                            }
+                        }
+                    }
+                ]
+            }
+        }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'build'));
